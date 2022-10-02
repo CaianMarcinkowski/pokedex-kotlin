f81a62b5b8e3e8cfab10a1ae1dfd26e6652eed14
Caian marcinkowski <caianferre@gmail.com>
2022-10-01 23:04:37 -0300

    Change types colors and background red iquals pokedex

--- app/src/main/java/com/portfolio/pokedex_kotlin/view/MainActivity.kt
+++ app/src/main/java/com/portfolio/pokedex_kotlin/view/MainActivity.kt
@@ -2,18 +2,12 @@ package com.portfolio.pokedex_kotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Chronometer
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.portfolio.pokedex_kotlin.R
-import com.portfolio.pokedex_kotlin.api.PokemonRepository
-import com.portfolio.pokedex_kotlin.api.model.PokemonApiResult
 import com.portfolio.pokedex_kotlin.domain.Pokemon
-import com.portfolio.pokedex_kotlin.domain.PokemonType
 import com.portfolio.pokedex_kotlin.viewModel.PokemonViewModel
 import com.portfolio.pokedex_kotlin.viewModel.PokemonViewModelFactory
 
@@ -29,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 	  
 	  override fun onCreate(savedInstanceState: Bundle?) {
 			super.onCreate(savedInstanceState)
+			supportActionBar?.hide()
 			setContentView(R.layout.activity_main)
 			
 			recyclerView.layoutManager = LinearLayoutManager(this)
--- app/src/main/java/com/portfolio/pokedex_kotlin/view/PokemonAdapter.kt
+++ app/src/main/java/com/portfolio/pokedex_kotlin/view/PokemonAdapter.kt
@@ -1,6 +1,11 @@
 package com.portfolio.pokedex_kotlin.view
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Color.parseColor
+import android.graphics.drawable.Drawable
+import android.icu.number.IntegerWidth
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -40,13 +45,32 @@ class PokemonAdapter(
 				  
 				  item?.let {
 						Glide.with(itemView.context).load(it.imageUrl).into(ivPokemon)
-					 
+						
 						tvNumber.text = "N° ${item.formattedNumber}"
 						tvName.text = item.formattedName
+						
+						tvType1.setBackgroundColor(parseColor(CheckBackgroundColor(item.types[0].name.capitalize())))
+						
+						tvType1.setTextColor(
+							  parseColor(
+									CheckColorTextType(
+										  CheckBackgroundColor(
+												item.types[0].name.capitalize())))
+						)
+						
 						tvType1.text = item.types[0].name.capitalize()
-					 
+						
 						if (item.types.size > 1) {
 							  tvType2.visibility = View.VISIBLE
+							  tvType2.setBackgroundColor(parseColor(CheckBackgroundColor(item.types[1].name.capitalize())))
+							  
+							  tvType2.setTextColor(
+									parseColor(
+										  CheckColorTextType(
+												CheckBackgroundColor(
+													  item.types[1].name.capitalize())))
+							  )
+							  
 							  tvType2.text = item.types[1].name.capitalize()
 						} else {
 							  tvType2.visibility = View.GONE
@@ -55,5 +79,65 @@ class PokemonAdapter(
 				  
 			}
 			
+			private fun CheckBackgroundColor(nameType: String): String {
+				  return if (nameType == "Bug") {
+						"#729F3F"
+				  } else if (nameType == "Dark") {
+						"#707070"
+				  } else if (nameType == "Dragon") {
+						"#53A4Cf"
+				  } else if (nameType == "Electric") {
+						"#EED535"
+				  } else if (nameType == "Fairy") {
+						"#FDB9E9"
+				  } else if (nameType == "Fighting") {
+						"#D56723"
+				  } else if (nameType == "Fire") {
+						"#FD7D24"
+				  } else if (nameType == "Flying") {
+						"#3DC7Ef"
+				  } else if (nameType == "Ghost") {
+						"#7B62A3"
+				  } else if (nameType == "Grass") {
+						"#9BCC50"
+				  } else if (nameType == "Ground") {
+						"#F7DE3F"
+				  } else if (nameType == "Ice") {
+						"#51C4E7"
+				  } else if (nameType == "Poison") {
+						"#B97FC9"
+				  } else if (nameType == "Psychic") {
+						"#F366B9"
+				  } else if (nameType == "Rock") {
+						"#A38C21"
+				  } else if (nameType == "Steel") {
+						"#A38C21"
+				  } else if (nameType == "Water") {
+						"#4592C4"
+				  } else {
+						//If is Normal return this color
+						"#A4ACAF"
+				  }
+			}
+			
+			private fun CheckColorTextType(hexaBackground: String): String {
+				  
+				  val colorHexadecimal = Color.parseColor(hexaBackground)
+				  
+				  val r = Color.red(colorHexadecimal)
+				  val g = Color.green(colorHexadecimal)
+				  val b = Color.blue(colorHexadecimal)
+				  
+				  val luminositi: Int = (r.times(299) + g.times(587) + b.times(114)).floorDiv(1000)
+				  
+				  return if (luminositi < 175) {
+						"#FFFFFF"
+				  } else {
+						"#000000"
+				  }
+				  
+			}
 	  }
+	  
 }
+
